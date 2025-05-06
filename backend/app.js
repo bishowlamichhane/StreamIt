fd82b98a1a856c509568f6b5ebe2dfbf496ba64a
bishowlamichhane <lamichhanebisho4545@gmail.com>
2025-05-06 20:22:02 +0545

    origin path for corssss

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const _dirname = path.resolve();
 
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin:process.env.CORS_ORIGIN,
     credentials:true,
 
 }))
